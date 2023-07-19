63c618fcf02e2e0c9a732b03fcb52a6d4d493d40
Resa Irsanto <resairsantoo@gmail.com>
2023-07-19 15:39:26 +0700

    create login page and register page

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import LoginPage from './pages/LoginPage'
+// import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <LoginPage />
+    </>
+  )
+}
+
+export default App
---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react"
+
+function RegisterPage() {
+   const [form, setForm] = useState({
+      email: '',
+      password: ''
+   })
+
+   function handleChange(e) {
+      setForm({
+         ...form,
+         [e.target.name]: e.target.value
+      })
+   }
+
+   return (
+      <>
+         <div className="container top-margin">
+            <div className="row justify-content-center">
+               <div className="col-md-5 bg-white shadow pt-3 pb-3 rounded-1 ps-5 pe-5">
+                  <div className="text-center custom-font mt-4">
+                     <h3>SIGN <span className="text-primary">IN</span></h3>
+                  </div>
+                  <form>
+                     <div className="mb-3">
+                        <label htmlFor="exampleInputEmail1" className="form-label custom-font" >Email address</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="example@mail.com" name="email" onChange={handleChange} />
+                     </div>
+                     <div className="mb-3">
+                        <label htmlFor="exampleInputPassword1" className="form-label custom-font">Password</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1" name="password" placeholder="******" onChange={handleChange} />
+                     </div>
+                     <button type="submit" className="form-control btn btn-primary mb-3">Submit</button>
+                  </form>
+                  <div className="text-center mb-3">
+                     Not a member? Register
+                  </div>
+               </div>
+            </div>
+         </div>
+      </>
+   )
+}
+
+export default RegisterPage
